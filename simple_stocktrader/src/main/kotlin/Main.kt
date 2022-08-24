45c16de66be70112ca10adc7fe0a76d087210c66
ksvedal <kristoffersvedal@gmail.com>
2022-08-24 18:24:47 +0100

    working stock algorithm.

--- simple_stocktrader/src/main/kotlin/Main.kt
+++ simple_stocktrader/src/main/kotlin/Main.kt
@@ -1,16 +1,42 @@
 
-val prices = arrayOf( -1, 3, -9, 2, 2, -1, 2, -1, -5 )
-
-var rounds = 0
-
+val prices = arrayOf( -1, 30, -9, 2, 2, -1, 2, 100, -5 )
 
 fun main() {
     calculateMaxProfit()
 }
 
 fun calculateMaxProfit() {
-    var high = 0
-    var low = 0
+    var highPrice = 0
+    var lowPrice = 0
 
-    prices.forEach { println(it) }
+    var temporaryLowDay = 0
+
+    var lowDay = 0
+    var highDay = 0
+
+    var profit = 0
+
+    prices.forEachIndexed() { index, price ->
+
+        if (index == 0) {
+            println("Current price: $price")
+            lowPrice = price
+        }
+
+        if (price < lowPrice) {
+            lowPrice = price;
+            temporaryLowDay = index+1
+        } else {
+            if ((highPrice < highPrice - lowPrice) && (index > temporaryLowDay - 1) && (index > 0)) {
+                highPrice = price
+
+                if (profit < (highPrice - lowPrice)) {
+                    profit = highPrice - lowPrice
+                    highDay = index + 1
+                    lowDay = temporaryLowDay
+                }
+            }
+        }
+    }
+    println("To make the highest profit, buy at $lowDay and sell at $highDay for a profit of $profit")
 }
