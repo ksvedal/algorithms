c2cdfcaddafc3fe6169c0a432e63f9551da78426
ksvedal <kristoffersvedal@gmail.com>
2022-08-24 19:28:05 +0100

    now it's working.

--- simple_stocktrader/src/main/kotlin/Main.kt
+++ simple_stocktrader/src/main/kotlin/Main.kt
@@ -1,38 +1,47 @@
 
-val prices = arrayOf( -1, 30, -9, 2, 2, -1, 2, 100, -5 )
+val prices = arrayOf( -12, 30, -1, 2, 100, -1000, 2222, 1 )
 
 fun main() {
     calculateMaxProfit()
 }
 
 fun calculateMaxProfit() {
-    var highPrice = 0
+
+    var temporaryLowDay = 1
+    var lowDay = 1
+    var highDay = 1
+
+    var currentPrice = 0
     var lowPrice = 0
-
-    var temporaryLowDay = 0
-
-    var lowDay = 0
-    var highDay = 0
-
+    var highPrice = 0
     var profit = 0
 
-    prices.forEachIndexed() { index, price ->
+    prices.forEachIndexed() { day, priceChange ->
 
-        if (index == 0) {
-            println("Current price: $price")
-            lowPrice = price
+        // Set low and high price of the first day.
+        if (day == 0) {
+            lowPrice = priceChange
+            highPrice = priceChange
         }
 
-        if (price < lowPrice) {
-            lowPrice = price;
-            temporaryLowDay = index+1
-        } else {
-            if ((highPrice < highPrice - lowPrice) && (index > temporaryLowDay - 1) && (index > 0)) {
-                highPrice = price
+        // Change current price of stock.
+        currentPrice += priceChange
 
+        // Sets temporary low day if current price is less than the lowest recorded price.
+        if (currentPrice < lowPrice) {
+            lowPrice = currentPrice;
+            temporaryLowDay = day + 1
+        } else {
+
+            if ((highPrice < highPrice - lowPrice) && (day > temporaryLowDay - 1) && (day > 0)) {
+
+                // Sets high price if it's less than current price
+                if (highPrice < currentPrice) highPrice = currentPrice
+
+                // Sets profit, low day and high day if profit is higher than currently registered
                 if (profit < (highPrice - lowPrice)) {
                     profit = highPrice - lowPrice
-                    highDay = index + 1
+                    highDay = day + 1
                     lowDay = temporaryLowDay
                 }
             }
