e217b03aab4a9de592e5b242bf0691ec6277ce2a
ksvedal <kristoffersvedal@gmail.com>
2022-10-05 02:35:38 +0100

    Cleaned up and did some javadoc.

--- string_hashtable/src/Main.java
+++ string_hashtable/src/Main.java
@@ -1,70 +1,87 @@
 import java.io.File;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         float count = 0;
-        int size = 128;
+        int size = 127;
         HashTable hashTable = new HashTable(size);
         File file = new File("navn.txt");
 
         // Read the file and add all the names into the hash table.
         try (Scanner sc = new Scanner(new File(String.valueOf(file)))){
             while (sc.hasNext()){
-                Name name = new Name(sc.nextLine());
-                hashTable.Add(name);
+                hashTable.addName(sc.nextLine());
                 count++;
             }
         } catch (Exception e){
             e.printStackTrace();
         }
 
-        // Search for my own name (no collision)
-        if (search == null){
-            System.out.println("No matching names with search.");
-        } else {
-            System.out.println(search);
-        }
+        hashTable.printTable();
 
-        // Search for a name with collision
+        // Search for a name with multiple names in the same index.
+        System.out.println("\n");
+        Name search = hashTable.findName("[REDACTED]");
         if (search == null){
             System.out.println("No matching names with search.");
         } else {
-            System.out.println(search);
+            System.out.println("Found name: " + search);
         }
+        System.out.println();
 
         // Print number of people in list, collisions and load factor.
-        System.out.println("\nNumber of people: " + count);
+        System.out.println("Number of people: " + count);
         System.out.println("Collisions: " + hashTable.collisions);
         System.out.println("Collisions per person: " + (hashTable.collisions / count));
         System.out.println("Load factor: " + (count / size));
     }
 }
 
+/**
+ * A table of names indexed by hashing.
+ * If duplicate hashed values, the duplicated names will link to each other.
+ */
 class HashTable {
     Name[] table;
     int collisions = 0;
 
+    /**
+     * Hash table of names.
+     * @param size Size of table.
+     */
     public HashTable(int size){
         this.table = new Name[size];
     }
 
-    public void Add(Name name){
-        int index = name.hash(table.length);
+    /**
+     * Adds name to the hash table using hashing function.
+     * If the index hashed value is a duplicate, will take the place and link to the old one.
+     * @param nameToAdd Name to add.
+     */
+    public void addName(String nameToAdd){
+        Name name = new Name(nameToAdd);
+        int index = name.hashName(table.length);
 
         // If there is already a value at the key spot, link the old one to this.
         if (table[index] != null){
             collisions++;
-            System.out.println(name + " Collided with " + table[index].toString());
+            System.out.println(nameToAdd + " Collided with " + table[index].toString());
             name.linkNext(table[index]);
         }
         table[index] = name;
     }
 
-    public Name findName(String name){
-        Hash hash = new Hash();
+    /**
+     * Finds exact name by string.
+     * @param nameToSearch Name to search for.
+     * @return null if no match. String of name if matched.
+     */
+    public Name findName(String nameToSearch){
+        Name name = new Name(nameToSearch);
         // Hash the searched name to get its key value
-        int hashValue = hash.multiplicationHash(name, 17, table.length);
+        int hashValue = name.hashName(table.length);
 
         // If there is nothing at key value, return null.
         if (table[hashValue] == null){
@@ -72,7 +89,7 @@ class HashTable {
         } else {
 
             // Otherwise, try if the first name in the spot of the key value equals the search term.
-            if (table[hashValue].getName().equals(name)){
+            if (table[hashValue].getName().equals(nameToSearch)){
                 return table[hashValue];
             } else {
 
@@ -80,7 +97,7 @@ class HashTable {
                 Name check = table[hashValue];
                 while (check.next != null) {
                     System.out.println("The search collided with: " + check);
-                    if (check.next.getName().equals(name)){
+                    if (check.next.getName().equals(nameToSearch)){
                         return check.next;
                     } else {
                         check = check.next;
@@ -90,57 +107,74 @@ class HashTable {
         }
         return null;
     }
+
+    /**
+     * Prints the entire table with indicators for the names that point to other names.
+     */
+    public void printTable() {
+        int i = 1;
+        for (Name name : table) {
+            Name check = name;
+            System.out.print("\n   On index: " + i + ": ");
+            i++;
+            if (name != null) {
+                System.out.print(name);
+                while (check.next != null) {
+                    check = check.next;
+                    System.out.print(",      -->       " + check);
+                }
+            }
+        }
+    }
 }
 
 /**
- * Contains the name of a person as string, its hash value, and the next name in line (making it work
+ * Contains the name of a person as string and the next name in line (making it work
  * like a singly linked list in case of collisions).
  */
 class Name {
     private final String name;
-    private final Hash hash = new Hash();
     Name next;
 
+    /**
+     * The full name of a person.
+     * @param name Name of a person.
+     */
     public Name(String name){
         this.name = name;
     }
 
+    /**
+     * Returns name
+     * @return Name.
+     */
     public String getName() {
         return name;
     }
 
-    public int hash(int size){
-        return hash.multiplicationHash(this.name,17, size);
-    }
-
+    /**
+     * Links the next name to this.
+     * @param next name to link.
+     */
     public void linkNext(Name next){
         this.next = next;
+    }
+
+    /**
+     * Modular hashing function using a prime number to avoid common factors between different numbers.
+     * @param size Size of table.
+     * @return Number result from hashing the name.
+     */
+    public int hashName(int size) {
+        int hash = 0;
+        for (int i = 0; i < this.name.length(); i++) {
+            hash = (11 * hash + this.name.charAt(i)) % size;
+        }
+        return hash;
     }
 
     @Override
     public String toString() {
         return this.name;
-    }
-}
-
-class Hash {
-    static int A = 1327217885;
-
-    public int getIntFromString(String name){
-        int sum = 0;
-        for (int i = 0; i < name.length(); i++) {
-            int c = name.charAt(i);
-            sum += (i*100)*c;
-        }
-        return sum;
-    }
-
-    public int multiplicationHash(String name, int x, int size){
-        System.out.println(getIntFromString(name) * A >> (32-x) & (size - 1));
-        return getIntFromString(name) * A >> (32-x) & (size - 1);
-    }
-
-    public int restDivisionHash(String name, int size){
-        return (getIntFromString(name) % size);
     }
 }
