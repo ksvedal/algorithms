8ff1c13f86b9b3c4943056e2ff2de8de3d254870
ksvedal <kristoffersvedal@gmail.com>
2022-11-09 23:03:59 +0100

    i think im done almost.

--- lempel_ziv_compression/src/LempelZiv.java
+++ lempel_ziv_compression/src/LempelZiv.java
@@ -3,7 +3,100 @@ import java.util.ArrayList;
 
 public class LempelZiv {
 
-    private final int MIN_LENGTH = 3;
+    private final int minimumLength = 3;
+
+    public void compress(String from, String to) throws IOException {
+
+        DataInputStream inputStream = new DataInputStream(new BufferedInputStream(new FileInputStream(from)));
+        DataOutputStream outputStream = new DataOutputStream(new FileOutputStream(to));
+
+        // puts the input as a byte block into a byteArray represented by its decimal value (-128 - 127)
+        byte[] fileAsBytes = inputStream.readAllBytes();
+
+        ArrayList<Byte> compressed = new ArrayList<>();
+        byte unchanged = minimumLength;
+        int i;
+
+        // Loops until end of the byte Array
+        for (i = minimumLength; i < fileAsBytes.length; i++) {
+            boolean foundInDictionary = false;
+            short distanceBuffer = 0;
+            byte foundLengthByte = 0;
+
+            // Loops until the max value of the Short datatype (32 767)
+            for (short temporaryDistance = 3; temporaryDistance <= i && temporaryDistance < Short.MAX_VALUE; temporaryDistance++) {
+
+                // Checks if byte in current index of bytearray is found in dictionary
+                if (fileAsBytes[i - temporaryDistance] == fileAsBytes[i]) {
+                    int foundLengthInt;
+
+                    for (foundLengthInt = 1; foundLengthInt < temporaryDistance && foundLengthInt < Byte.MAX_VALUE && i + foundLengthInt < fileAsBytes.length;) {
+
+                        // Finds the chars in dictionary and checks how long it lasts
+                        if (fileAsBytes[i + foundLengthInt] == fileAsBytes[i - temporaryDistance + foundLengthInt]) {
+                            foundLengthInt++;
+                        }
+                        else {
+                            break;
+                        }
+
+                    }
+                    if (foundLengthInt > foundLengthByte && foundLengthInt > minimumLength) {
+                        foundInDictionary = true;
+                        foundLengthByte = (byte)foundLengthInt;
+                        distanceBuffer = temporaryDistance;
+                        if (foundLengthByte == Byte.MAX_VALUE) {
+                            break;
+                        }
+                    }
+                }
+            }
+            if (foundInDictionary) {
+                if (unchanged != 0) {
+                    compressed.add(unchanged);
+                    for (int j = i - unchanged; j < i; j++) {
+                        compressed.add(fileAsBytes[j]);
+                    }
+                    unchanged = 0;
+                }
+
+                // Adds the negative signed byte value signaling that it has been compressed.
+                compressed.add((byte)(-foundLengthByte));
+
+                // Divides the distance short into two bytes and adds one byte at a time.
+                compressed.add((byte)(distanceBuffer & 0xff));        // First half
+                compressed.add((byte)((distanceBuffer >> 8) & 0xff)); // Second half
+
+                i += ((int)(foundLengthByte) - 1);
+            }
+            else {
+                unchanged++;
+            }
+
+            // If unchanged byte buffer fills up, add them to the compressed file and clear the buffer.
+            if (unchanged == Byte.MAX_VALUE) {
+                compressed.add(unchanged);
+                for (int j = i - unchanged + 1; j <= i; j++) {
+                    compressed.add(fileAsBytes[j]);
+                }
+                unchanged = 0;
+            }
+        }
+
+        // Add the unchanged byte buffer to the end of the compressed file (if there is any left).
+        compressed.add(unchanged);
+        for (int j = i - unchanged; j < i; j++) {
+            compressed.add(fileAsBytes[j]);
+        }
+
+        inputStream.close();
+
+        for (Byte b : compressed)
+            outputStream.write(b);
+
+        outputStream.flush();
+        outputStream.close();
+    }
 
     public void decompress(String from, String to) throws IOException {
         DataInputStream inputStream = new DataInputStream(new BufferedInputStream(new FileInputStream(from)));
@@ -13,38 +106,39 @@ public class LempelZiv {
 
         ArrayList<Byte> decompressed = new ArrayList<>();
 
-        int indexOut = 0;
+        int currentIndex = 0;
+        int totalIndex = 0;
 
         // Iterates over all the data bytes.
-        for (int i = 0; i < data.length; i++) {
+        for (totalIndex = 0; totalIndex < data.length; totalIndex++) {
 
             // x = byte in index
-            byte x = data[i];
+            byte x = data[totalIndex];
 
-            // If the byte is negative
-            // Meaning its been compressed
+            // If the byte is negative signals that the next two bytes have been compressed.
             if (x < 0) {
-                // The distance of the compressed section
-                short distance = (short)((data[i + 1] & 0xff) | ((data[i + 2] & 0xff) << 8));
+                // Combines the next two bytes into a short.
+                short distance = (short)((data[totalIndex + 1] & 0xff) | ((data[totalIndex + 2] & 0xff) << 8));
                 // Starts at the start
-                int start = indexOut;
+                int start = currentIndex;
 
+                // Adds the bytes from the dictionary (j) to the decompressed ArrayList.
                 for (int j = start - distance; j < start - distance - x; j++) {
                     decompressed.add(decompressed.get(j));
-                    indexOut++;
+                    currentIndex++;
                 }
 
                 // Adds the size of the byte section to index for the next iteration.
-                i += (MIN_LENGTH - 1);
+                totalIndex += (minimumLength - 1);
             }
 
-            // If the byte is positive
+            // If the byte is positive (a repeated sequence) just adds the byte.
             else {
-                for (int j = i + 1; j <= i + x && j < data.length; j++) {
+                for (int j = totalIndex + 1; j <= totalIndex + x && j < data.length; j++) {
                     decompressed.add(data[j]);
-                    indexOut++;
+                    currentIndex++;
                 }
-                i += x;
+                totalIndex += x;
             }
         }
 
--- lempel_ziv_compression/src/Main.java
+++ lempel_ziv_compression/src/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) throws IOException {
         LempelZiv lempelZiv = new LempelZiv();
 
-        String original = "test.txt";
+        String original = "test.lyx";
         String compressed = "test2";
-        String deCompressed = "test3.txt";
+        String deCompressed = "test3.lyx";
 
         lempelZiv.compress(original, compressed);
         lempelZiv.decompress(compressed, deCompressed);
