f91aa3258c6aaa1055b6e4975ab43beded66680e
ksvedal <kristoffersvedal@gmail.com>
2022-11-10 22:11:24 +0100

    commented out the 4 block byte code.

--- lempel_ziv_compression/src/LempelZiv.java
+++ lempel_ziv_compression/src/LempelZiv.java
@@ -10,7 +10,7 @@ public class LempelZiv {
         DataInputStream inputStream = new DataInputStream(new BufferedInputStream(new FileInputStream(from)));
         DataOutputStream outputStream = new DataOutputStream(new FileOutputStream(to));
 
-        // puts the input as a byte block into a byteArray represented by its decimal value (-128 - 127)
+        // puts the input as a byte block into a byteArray represented by its decimal value (-128 to 127)
         byte[] fileAsBytes = inputStream.readAllBytes();
 
         ArrayList<Byte> compressed = new ArrayList<>();
@@ -19,34 +19,34 @@ public class LempelZiv {
 
         // Until end of the byte Array
         while (i < fileAsBytes.length) {
-            boolean foundInDictionary = false;
+            boolean foundMatch = false;
             short distanceBuffer = 0;
             byte foundLengthByte = 0;
             short temporaryDistance = 3;
 
-            // Loops until the max value of the Short datatype (32 767)
+            // Until the temp distance reaches the index or the max value of the Short datatype (32 767)
             while (temporaryDistance <= i && temporaryDistance < Short.MAX_VALUE) {
 
-                // Checks if byte in current index of bytearray is found in dictionary
+                // Checks if byte in current distance has match with the current index
                 if (fileAsBytes[i - temporaryDistance] == fileAsBytes[i]) {
-                    int foundLengthInt;
+                    int matchLength = 1;
 
-                    for (foundLengthInt = 1; foundLengthInt < temporaryDistance && foundLengthInt < Byte.MAX_VALUE && i + foundLengthInt < fileAsBytes.length;) {
+                    // Until max value of a byte (127)
+                    while (matchLength < temporaryDistance && matchLength < Byte.MAX_VALUE && i + matchLength < fileAsBytes.length) {
 
-                        // Finds the chars in dictionary and checks how long it lasts
-                        if (fileAsBytes[i + foundLengthInt] == fileAsBytes[i - temporaryDistance + foundLengthInt]) {
-                            foundLengthInt++;
+                        // checks the two bytes and adds to the match length unless they differ.
+                        if (fileAsBytes[i + matchLength] == fileAsBytes[matchLength + (i - temporaryDistance)]) {
+                            matchLength++;
                         }
                         else {
                             break;
                         }
-
                     }
 
-                    // Tags the current bytes as in the dictionary and adds foundLengthByte and distanceBuffer.
-                    if (foundLengthInt > foundLengthByte && foundLengthInt > minimumLength) {
-                        foundInDictionary = true;
-                        foundLengthByte = (byte)foundLengthInt;
+                    // Tags the current bytes as match and adds foundLengthByte and distanceBuffer.
+                    if (matchLength > foundLengthByte && matchLength > minimumLength) {
+                        foundMatch = true;
+                        foundLengthByte = (byte)matchLength;
                         distanceBuffer = temporaryDistance;
                         if (foundLengthByte == Byte.MAX_VALUE) {
                             break;
@@ -55,12 +55,14 @@ public class LempelZiv {
                 }
                 temporaryDistance++;
             }
-            if (foundInDictionary) {
+
+            // If there has been a match between two bytes
+            if (foundMatch) {
+
+                // Firstly empties the unchanged bytes into the compressed file.
                 if (unchanged != 0) {
                     compressed.add(unchanged);
-                    for (int j = i - unchanged; j < i; j++) {
-                        compressed.add(fileAsBytes[j]);
-                    }
+                    for (int j = i - unchanged; j < i; j++) compressed.add(fileAsBytes[j]);
                     unchanged = 0;
                 }
 
@@ -70,8 +72,10 @@ public class LempelZiv {
                 // Divides the distance short into four bytes and adds one byte to the compressed ArrayList at a time.
                 compressed.add((byte)(distanceBuffer & 0xff));        // First half
                 compressed.add((byte)((distanceBuffer >> 8) & 0xff)); // Second half
-                // compressed.add((byte)((distanceBuffer >> 16) & 0xff));
-                // compressed.add((byte)((distanceBuffer >> 24) & 0xff));
+
+                // The two next lines is if we use int ( 4 byte blocks ) which made the implementation worse.
+                    // compressed.add((byte)((distanceBuffer >> 16) & 0xff));
+                    // compressed.add((byte)((distanceBuffer >> 24) & 0xff));
 
                 i += ((int)(foundLengthByte) - 1);
             } else {
@@ -89,7 +93,7 @@ public class LempelZiv {
             i++;
         }
 
-        // Add the unchanged byte buffer to the end of the compressed file (if there is any left).
+        // In the end, add the unchanged bytes to the end of the compressed file (if there is any left).
         compressed.add(unchanged);
         for (int j = i - unchanged; j < i; j++) {
             compressed.add(fileAsBytes[j]);
@@ -124,38 +128,39 @@ public class LempelZiv {
 
             // If the byte is positive (a repeated sequence) just adds the byte.
             if (currentByte >= 0) {
-                int j = totalIndex + 1;
-                while (j <= totalIndex + currentByte && j < fileAsBytes.length) {
-                    decompressed.add(fileAsBytes[j]);
+                int i = totalIndex + 1;
+                while (i <= totalIndex + currentByte && i < fileAsBytes.length) {
+                    decompressed.add(fileAsBytes[i]);
                     currentIndex++;
-                    j++;
+                    i++;
                 }
-                totalIndex += currentByte;
+                totalIndex += currentByte+1;
             }
 
-            // Otherwise, if the byte is negative, signals that the next two bytes have been compressed.
-            // currentByte then acts as a pointer to that part of the dictionary.
+            // Otherwise, if the byte is negative, signals that there is a match hit.
+            // currentIndex and distance then indicates the reference.
             else {
-                // Combines the next two bytes into a short.
 
+                // Combines the next two bytes into a short.
+                distance = (short)((fileAsBytes[totalIndex + 1] & 0xff) | ((fileAsBytes[totalIndex + 2] & 0xff) << 8));
+
+                // The next line is if we use int ( 4 byte blocks ) which made the implementation worse.
                 // distance = (int)((fileAsBytes[totalIndex + 1] & 0xff) | ((fileAsBytes[totalIndex + 2] & 0xff) << 8) | ((fileAsBytes[totalIndex + 3] & 0xff) << 16) | ((fileAsBytes[totalIndex + 4] & 0xff) << 24));
 
-                distance = (short)((fileAsBytes[totalIndex + 1] & 0xff) | ((fileAsBytes[totalIndex + 2] & 0xff) << 8));
-                // Starts at the start
+                // Start
                 int start = currentIndex;
-                int j = currentIndex - distance;
+                int i = currentIndex - distance;
 
-                // Adds the bytes from the dictionary (j) to the decompressed ArrayList.
-                while (j < start - distance - currentByte) {
-                    decompressed.add(decompressed.get(j));
+                // Adds the bytes that has been pointed to.
+                while (i < start - distance - currentByte) {
+                    decompressed.add(decompressed.get(i));
                     currentIndex++;
-                    j++;
+                    i++;
                 }
 
                 // Adds the size of the byte section to index for the next iteration.
-                totalIndex += (minimumLength - 1);
+                totalIndex += (minimumLength);
             }
-            totalIndex++;
         }
 
         // Writes the decompressed bytes to file.
@@ -166,5 +171,4 @@ public class LempelZiv {
         outputStream.flush();
         outputStream.close();
     }
-
 }
--- lempel_ziv_compression/src/Main.java
+++ lempel_ziv_compression/src/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) throws IOException {
         LempelZiv lempelZiv = new LempelZiv();
 
-        String original = "oppgavetekst.pdf";
-        String compressed = "compressed_oppgavetekst_pdf";
-        String deCompressed = "decompressed_oppgavetekst.pdf";
+        String original = "forelesning.lyx";
+        String compressed = "compressed_forelesning.lyx";
+        String deCompressed = "decompressed_forelesning.lyx";
 
         lempelZiv.compress(original, compressed);
         lempelZiv.decompress(compressed, deCompressed);
